e94f09477ba3d006ed6cd5dfaac653aa3e1443ba
Antonio Salazar <inewgen@hotmail.com>
2022-06-13 18:58:22 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicios01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mi mundo!");
+        }
+    }
+}
---
+++ SumadeNumeros/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SumadeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 19;
+            int b= 28;
+            Console.WriteLine("La suma es!" + a + b);
+        }
+    }
+}
